dfd928ac15fb5cfe6711a5dcdf77e3defe5056fb
bhkgy <167137931+bhkgy@users.noreply.github.com>
2024-07-10 05:34:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,79 +1,79 @@
-const キャンバス = document.getElementById("ゲームキャンバス");
-const コンテキスト = キャンバス.getContext("2d");
-const ボックス = 20;
-const キャンバスサイズ = キャンバス.width;
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+const box = 20;
+const canvasSize = canvas.width;
 
-let ヘビ = [{ x: ボックス * 5, y: ボックス * 5 }];
-let 方向 = "右";
-let 食べ物 = {
-    x: Math.floor(Math.random() * キャンバスサイズ / ボックス) * ボックス,
-    y: Math.floor(Math.random() * キャンバスサイズ / ボックス) * ボックス,
+let snake = [{ x: box * 5, y: box * 5 }];
+let direction = "RIGHT";
+let food = {
+    x: Math.floor(Math.random() * canvasSize / box) * box,
+    y: Math.floor(Math.random() * canvasSize / box) * box,
 };
-const リンゴ画像 = new Image();
-リンゴ画像.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABhElEQVR42q2SMU/TUBCG/7n5gtRQpKRhLoZKSkhShEKVbBRG2WSbSjkHjwGl7Gxotugow8ldY3oYiSwU6WeRDaiA0UjR06GgTTovLBV1DUrb1bbpfXs0EN7nd8/j8zv3vff7QxtMOve2EYAxyKcX/ulSTydhZxAvROAioLRb5T8rwUBiAIAZVQwrgx8WI8HyCPw1b3WiKyNqkRcuIr1VJWpN5ELrdvI1prcKKRtQiKpEDACs5QwE0AMMn3H3a4k+E1+KbQWsZbuNRXKNNp8IEuEn0zi0XmvszbI2tsJrL7S20TiHpBSPYZDOjgzj2EnqsRYagZFtBbBsg4fKOIQqO53B0Hk8PzhjLpqBf9ONuYWhBvlZ1yYhsc+J/yb1/g82YJfH9A5Q0P1L8u+kBO2e0NiyLkMN2q7SoLDp77G19K+pgeBAk/gCyfaOcsPVZOV9AMAzXzIKrAwCvj+yxpaeVpqd0KgJpRQm2K0RCPTXsl9tfDkJS4ocJH1omLO+5qRwvHJ+UQ4gWhl/UfYX6YnddBBHkmNYAAAAASUVORK5CYII=';
+const appleImage = new Image();
+appleImage.src = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAABhElEQVR42q2SMU/TUBCG/7n5gtRQpKRhLoZKSkhShEKVbBRG2WSbSjkHjwGl7Gxotugow8ldY3oYiSwU6WeRDaiA0UjR06GgTTovLBV1DUrb1bbpfXs0EN7nd8/j8zv3vff7QxtMOve2EYAxyKcX/ulSTydhZxAvROAioLRb5T8rwUBiAIAZVQwrgx8WI8HyCPw1b3WiKyNqkRcuIr1VJWpN5ELrdvI1prcKKRtQiKpEDACs5QwE0AMMn3H3a4k+E1+KbQWsZbuNRXKNNp8IEuEn0zi0XmvszbI2tsJrL7S20TiHpBSPYZDOjgzj2EnqsRYagZFtBbBsg4fKOIQqO53B0Hk8PzhjLpqBf9ONuYWhBvlZ1yYhsc+J/yb1/g82YJfH9A5Q0P1L8u+kBO2e0NiyLkMN2q7SoLDp77G19K+pgeBAk/gCyfaOcsPVZOV9AMAzXzIKrAwCvj+yxpaeVpqd0KgJpRQm2K0RCPTXsl9tfDkJS4ocJH1omLO+5qRwvHJ+UQ4gWhl/UfYX6YnddBBHkmNYAAAAASUVORK5CYII=';
 
-document.addEventListener("keydown", 方向を設定);
+document.addEventListener("keydown", setDirection);
 
-function 方向を設定(event) {
-    if (event.keyCode === 37 && 方向 !== "右") 方向 = "左";
-    else if (event.keyCode === 38 && 方向 !== "下") 方向 = "上";
-    else if (event.keyCode === 39 && 方向 !== "左") 方向 = "右";
-    else if (event.keyCode === 40 && 方向 !== "上") 方向 = "下";
+function setDirection(event) {
+    if (event.keyCode === 37 && direction !== "RIGHT") direction = "LEFT";
+    else if (event.keyCode === 38 && direction !== "DOWN") direction = "UP";
+    else if (event.keyCode === 39 && direction !== "LEFT") direction = "RIGHT";
+    else if (event.keyCode === 40 && direction !== "UP") direction = "DOWN";
 }
 
-function ゲームを描画() {
-    コンテキスト.clearRect(0, 0, キャンバスサイズ, キャンバスサイズ);
+function drawGame() {
+    ctx.clearRect(0, 0, canvasSize, canvasSize);
 
-    for (let i = 0; i < ヘビ.length; i++) {
-        コンテキスト.fillStyle = i === 0 ? "green" : "white";
-        コンテキスト.fillRect(ヘビ[i].x, ヘビ[i].y, ボックス, ボックス);
-        コンテキスト.strokeStyle = "red";
-        コンテキスト.strokeRect(ヘビ[i].x, ヘビ[i].y, ボックス, ボックス);
+    for (let i = 0; i < snake.length; i++) {
+        ctx.fillStyle = i === 0 ? "green" : "white";
+        ctx.fillRect(snake[i].x, snake[i].y, box, box);
+        ctx.strokeStyle = "red";
+        ctx.strokeRect(snake[i].x, snake[i].y, box, box);
 
         if (i === 0) {
-            コンテキスト.fillStyle = "black";
-            コンテキスト.beginPath();
-            コンテキスト.arc(ヘビ[i].x + ボックス / 2, ヘビ[i].y + ボックス / 2, ボックス / 4, 0, 2 * Math.PI);
-            コンテキスト.fill();
+            ctx.fillStyle = "black";
+            ctx.beginPath();
+            ctx.arc(snake[i].x + box / 2, snake[i].y + box / 2, box / 4, 0, 2 * Math.PI);
+            ctx.fill();
         }
     }
 
-    コンテキスト.drawImage(リンゴ画像, 食べ物.x, 食べ物.y, ボックス, ボックス);
+    ctx.drawImage(appleImage, food.x, food.y, box, box);
 
-    let ヘビX = ヘビ[0].x;
-    let ヘビY = ヘビ[0].y;
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
 
-    if (方向 === "左") ヘビX -= ボックス;
-    if (方向 === "上") ヘビY -= ボックス;
-    if (方向 === "右") ヘビX += ボックス;
-    if (方向 === "下") ヘビY += ボックス;
+    if (direction === "LEFT") snakeX -= box;
+    if (direction === "UP") snakeY -= box;
+    if (direction === "RIGHT") snakeX += box;
+    if (direction === "DOWN") snakeY += box;
 
-    if (ヘビX === 食べ物.x && ヘビY === 食べ物.y) {
-        食べ物 = {
-            x: Math.floor(Math.random() * キャンバスサイズ / ボックス) * ボックス,
-            y: Math.floor(Math.random() * キャンバスサイズ / ボックス) * ボックス,
+    if (snakeX === food.x && snakeY === food.y) {
+        food = {
+            x: Math.floor(Math.random() * canvasSize / box) * box,
+            y: Math.floor(Math.random() * canvasSize / box) * box,
         };
     } else {
-        ヘビ.pop();
+        snake.pop();
     }
 
-    let 新しい頭 = { x: ヘビX, y: ヘビY };
+    let newHead = { x: snakeX, y: snakeY };
 
-    if (ヘビX < 0 || ヘビY < 0 || ヘビX >= キャンバスサイズ || ヘビY >= キャンバスサイズ || 衝突(新しい頭, ヘビ)) {
-        clearInterval(ゲーム);
+    if (snakeX < 0 || snakeY < 0 || snakeX >= canvasSize || snakeY >= canvasSize || collision(newHead, snake)) {
+        clearInterval(game);
         alert("ゲームオーバー");
     }
 
-    ヘビ.unshift(新しい頭);
+    snake.unshift(newHead);
 }
 
-function 衝突(頭, array) {
+function collision(head, array) {
     for (let i = 0; i < array.length; i++) {
-        if (頭.x === array[i].x && 頭.y === array[i].y) {
+        if (head.x === array[i].x && head.y === array[i].y) {
             return true;
         }
     }
     return false;
 }
 
-let ゲーム = setInterval(ゲームを描画, 200);
+let game = setInterval(drawGame, 200);
